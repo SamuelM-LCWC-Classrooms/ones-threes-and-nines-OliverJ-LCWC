9030cc86a557a72a3b6df61297b6b7ab96dad91f
OliverJ-LCWC <144791858+OliverJ-LCWC@users.noreply.github.com>
2024-12-09 13:34:48 +0000

    done#

--- ones_threes_and_nines.py
+++ ones_threes_and_nines.py
@@ -1,5 +1,11 @@
 class Ones_threes_nines:
     def __init__(self, number: int):
-        self.ones = number % 1
-        self.threes = number % 3
-        self.nines = number % 9
+        self.ones = number // 1
+        self.threes = number // 3
+        self.nines = number // 9
+
+n1 = Ones_threes_nines(5)
+
+print(n1.ones)
+print(n1.threes)
+print(n1.nines)
